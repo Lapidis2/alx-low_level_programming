7a358c996580affd0ebbf37db936d70c4f8f54a5
HITAYEZU Jean Pierre <123136885+Lapidis2@users.noreply.github.com>
2023-02-18 00:46:00 +0200

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints numbers in ascending order
+ * Return: 0 if exited properly, non zero otherwise
+ */
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch++)
+{
+	for (n = 49; n <= 57; n++)
+	{
+		if (n > ch)
+		{
+			putchar(ch);
+			putchar(n);
+			if (ch != 56 || n != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+}
+putchar(10);
+return (0);
+}
