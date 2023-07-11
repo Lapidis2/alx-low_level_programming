362526a60e2ee65a33d17ad13bdf51f5448231ac
HITAYEZU Jean Pierre <123136885+Lapidis2@users.noreply.github.com>
2023-07-11 22:12:44 +0200

    updated codes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,38 +2,38 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * read_and_print_file - Reads a text file and prints its contents to stdout.
  * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * @max_chars: The maximum number of characters to read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ *         Otherwise - the actual number of characters read and printed.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_and_print_file(const char *filename, size_t max_chars)
 {
-	ssize_t o, r, w;
+	ssize_t file_descriptor, bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * max_chars);
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	file_descriptor = open(filename, O_RDONLY);
+	bytes_read = read(file_descriptor, buffer, max_chars);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (file_descriptor == -1 || bytes_read == -1 || bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(o);
+	close(file_descriptor);
 
-	return (w);
+	return (bytes_written);
 }
+
