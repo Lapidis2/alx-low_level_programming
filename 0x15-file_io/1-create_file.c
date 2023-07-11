8c5a84efe7fbb11819ec68a220e981e74cc34fe8
HITAYEZU Jean Pierre <123136885+Lapidis2@users.noreply.github.com>
2023-07-11 21:54:56 +0200

    new codes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,19 +10,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, leng = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (leng = 0; text_content[len];)
-			leng++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, leng);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
